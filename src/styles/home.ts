b8803528a1b58d70d5feb57d553a461f78a0ebcd
CODLuisP <esluiscabrera20@gmail.com>
2025-10-24 12:27:14 -0500

    Home cambios

--- src/styles/home.ts
+++ src/styles/home.ts
@@ -17,7 +17,7 @@ export const homeStyles = StyleSheet.create({
     right: 0,
     bottom: 0,
     width: '100%',
-    height: '100%',
+    height: 200,
     resizeMode: 'cover',
   },
   backgroundOverlay: {
