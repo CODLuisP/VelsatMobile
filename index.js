6f45e343b559b7859044fe0dfdd110edcde3ae13
CODLuisP <esluiscabrera20@gmail.com>
2025-11-06 12:29:23 -0500

    Nuevos estilos

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Text, TextInput } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+// 👇 AGREGA ESTAS LÍNEAS AQUÍ
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
+// 👆 FIN DE LAS LÍNEAS
 
 AppRegistry.registerComponent(appName, () => App);
