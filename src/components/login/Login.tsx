ce14990dc069cebec757cba381536d5f3f44e92c
CODLuisP <esluiscabrera20@gmail.com>
2025-11-07 15:16:30 -0500

    Nueva actualizacion 2.3.2

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -624,7 +624,7 @@ const Login = () => {
 
               <View style={styles.statusContainer}>
                 <View style={styles.statusDot} />
-                <Text style={styles.statusText}>V. 2.3.1</Text>
+                <Text style={styles.statusText}>V. 2.3.2</Text>
               </View>
             </View>
           </Animated.View>
