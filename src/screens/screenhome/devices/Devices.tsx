940321623d24bacb65669949840e79136db4b5b0
CODLuisP <esluiscabrera20@gmail.com>
2025-11-30 23:28:47 -0500

    cambio en device

--- src/screens/screenhome/devices/Devices.tsx
+++ src/screens/screenhome/devices/Devices.tsx
@@ -374,7 +374,7 @@ const Devices = () => {
     <View style={[styles.container, { paddingBottom: bottomSpace  }]}>
       {/* Header */}
       <LinearGradient
-        colors={['#031e4aff', '#0f2a75ff', '#1439a0ff']}
+        colors={['#05194fff', '#0f2a75ff', '#05194fff']}
       start={{ x: 0, y: 0 }}
       end={{ x: 0, y: 1 }}
         style={[styles.header, { paddingTop: topSpace }]}
