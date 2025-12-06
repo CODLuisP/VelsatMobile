e0617491387a4782249969b1e1670681aba2d38a
CODLuisP <esluiscabrera20@gmail.com>
2025-12-05 22:42:33 -0500

    cambios de version y map event

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -632,7 +632,7 @@ const Login = () => {
 
             {/* Version */}
             <View style={styles.versionContainer}>
-              <Text style={styles.versionText}>V. 2.3.9</Text>
+              <Text style={styles.versionText}>V. 2.4.0</Text>
             </View>
           </View>
         </Animated.View>
